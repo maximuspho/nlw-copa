e243bdb81afd0499eca6f9db51b5f5f0b12c7b53
Wilson Silveira <nene.cyber@hotmail.com>
2022-11-05 17:27:19 -0300

    adicionano title nas logo

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 function createGame(player1, hour, player2) {
     return `
-    <li>
-        <img src="./assets/icon-${player1}.svg" alt="Bandeira do ${player1}">
+    <li class="tito">
+        <img src="./assets/icon-${player1}.svg" alt="Bandeira do ${player1}" title="${player1}">
             <strong>${hour}</strong>
-        <img src="./assets/icon-${player2}.svg" alt="Bandeira da ${player2}">
+        <img src="./assets/icon-${player2}.svg" alt="Bandeira da ${player2}" title="${player2}">
     </li>
 
     `
@@ -31,19 +31,19 @@ document.querySelector("#cards").innerHTML = `
     ${createCard('21/11', 'Segunda', createGame('senegal', "13:00", 'holanda')
     + createGame('inglatera', "10:00", 'iran')
     + createGame('eua', "16:00", 'gales')
-    )}
+)}
 
     ${createCard('22/11', 'Terça', createGame('argentina', "07:00", 'arabiasaudita')
     + createGame('mexico', "13:00", 'polonia')
     + createGame('dinamarca', "10:00", 'tunisia')
     + createGame('franca', "16:00", 'australia')
-    )}
+)}
 
     ${createCard('23/11', 'Quarta', createGame('marrocos', "07:00", 'croacia')
     + createGame('espanha', "13:00", 'costarica')
     + createGame('alemanha', "10:00", 'japao')
     + createGame('belgica', "16:00", 'canada')
-    )}
+)}
 
     ${createCard('24/11', 'Quinta', createGame('suica', "07:00", 'camaroes')
     + createGame('uruguay', "10:00", 'coreiadosul')
